8fc87d13d985995f7bc92efb4697db4ebd3deaee
Nijesh W <nijesh.w.17@gmail.com>
2020-03-23 11:16:57 +0530

    Fix: Cross orgin issue

--- server.js
+++ server.js
@@ -9,23 +9,28 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 class Server {
-	constructor() {
-		this.port = process.env.PORT || 3000;
+    constructor() {
+        this.port = process.env.PORT || 3000;
         this.host = process.env.URL || 'localhost';
 
         this.app = express();
         this.http = http.Server(this.app);
         this.socket = socketio(this.http);
 
+        this.app.all('/', function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            next();
+        });
 
         this.app.get('/check', function (req, res) { // test route
             res.send('Welcome to Logezy chat API server')
         });
-	}
+    }
 
-	appRun(){
-		new socketEvents(this.socket).socketConfig();
-		this.app.use(express.static(__dirname + '/uploads'));
+    appRun() {
+        new socketEvents(this.socket).socketConfig();
+        this.app.use(express.static(__dirname + '/uploads'));
         this.http.listen(this.port, this.host, () => {
             console.log(`Logezy chat API server is on http://${this.host}:${this.port}`);
         });
