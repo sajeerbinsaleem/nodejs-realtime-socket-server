adcbc189c29edcdeea9c35c3e556be9d37456739
sajeerbinsaleem <sajeer.logezy@gmail.com>
2020-05-14 12:54:44 +0530

    Fixed : Push message condition

--- utils/helper.js
+++ utils/helper.js
@@ -48,14 +48,16 @@ class Helper{
 
 	async insertMessages(params){
 		try {
+			if(params.tenant_slug){
+				var firebase_server_key = await this.getFirebaseServerKey(params.tenant_slug,params.toUserId);
 
-        	var firebase_server_key = await this.getFirebaseServerKey(params.tenant_slug,params.toUserId);
-
-        	var firebase_uuid = await this.getFirebaseUid(params.tenant_slug, params.toUserId);
-
-			if(firebase_server_key != null && firebase_uuid != null ){
-				this.firebase.pushMessage(params.message, firebase_server_key, firebase_uuid);
+				var firebase_uuid = await this.getFirebaseUid(params.tenant_slug, params.toUserId);
+	
+				if(firebase_server_key != null && firebase_uuid != null ){
+					this.firebase.pushMessage(params.message, firebase_server_key, firebase_uuid);
+				}
 			}
+        	
 
 			var current_date = new Date();
 			
